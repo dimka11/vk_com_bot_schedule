4bbbb27ad4d42cdbed3ec7a7809a85127dd97215
Dmitry <dimka11lp@gmail.com>
2020-03-04 21:37:57 +0600

    9

--- index.js
+++ index.js
@@ -55,12 +55,22 @@
 // });
 
 
-const VkBot = require('node-vk-bot-api');
+// const VkBot = require('node-vk-bot-api');
 
+// const bot = new VkBot(process.env.TOKEN)
+
+// bot.command('/start', (ctx) => {
+//   ctx.reply('Hello!');
+// });
+
+// bot.startPolling();
+
+const VkBot = require('node-vk-bot-api')
+ 
 const bot = new VkBot(process.env.TOKEN)
-
+ 
 bot.command('/start', (ctx) => {
-  ctx.reply('Hello!');
-});
-
-bot.startPolling();
+  ctx.reply('Hello!')
+})
+ 
+bot.startPolling()
